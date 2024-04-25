4453d3dfedad78d9af2bb86f30a013a6fc9b3dce
bdmtnz <martinez_o@hotmail.es>
2024-04-25 13:59:14 -0500

    End department point

--- src/app/layout/pages/department/department.component.ts
+++ src/app/layout/pages/department/department.component.ts
@@ -8,11 +8,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { IDepartmentReport } from '../../../shared/models/department';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IApiResponse } from '../../../shared/models/api';
 
 @Component({
   selector: 'app-department',
   standalone: true,
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
@@ -25,44 +31,46 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
   templateUrl: './department.component.html',
   styleUrl: './department.component.scss'
 })
-export class DepartmentComponent implements AfterViewInit {
+export class DepartmentComponent implements AfterViewInit, OnInit {
+  formGroup: FormGroup
+
+  data: IDepartmentReport[] = []
   displayedColumns: string[] = ['departmentId', 'departmentName', 'sales'];
   dataSource: MatTableDataSource<IDepartmentReport>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor() {
-    let data: IDepartmentReport[] = [
-      {
-        departmentId: '1',
-        departmentName: 'Sales',
-        sales: 20000
-      },
-      {
-        departmentId: '1',
-        departmentName: 'Sales',
-        sales: 20000
-      },
-      {
-        departmentId: '1',
-        departmentName: 'System',
-        sales: 20000
-      },
-      {
-        departmentId: '1',
-        departmentName: 'X',
-        sales: 20000
-      },
-      {
-        departmentId: '1',
-        departmentName: 'Y',
-        sales: 80000
-      }
-    ]
-
+  constructor(
+    private readonly formBuilder: FormBuilder,
+    private readonly http: HttpClient
+  ) {
+    this.formGroup = this.formBuilder.group({
+      start: new FormControl({ value: new Date(), disabled: false }, { validators: [Validators.required] }),
+      end: new FormControl({ value: new Date(), disabled: false }, { validators: [Validators.required] })
+    })
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(data);
+    this.dataSource = new MatTableDataSource(this.data);
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  sendRequest() {
+    this.http.post<IApiResponse<Array<IDepartmentReport>>>(`${environment.apiUrl}/Department`, 
+      // {
+      //   "start": "2024-04-25T18:37:31.470Z",
+      //   "end": "2024-04-25T18:37:31.471Z"
+      // }
+      {
+        ...this.formGroup.value
+      }
+    ).subscribe(resp => {
+      this.data = resp.data
+      this.dataSource = new MatTableDataSource(this.data);
+      this.ngAfterViewInit()
+    })
   }
 
   ngAfterViewInit(): void {
@@ -77,5 +85,13 @@ export class DepartmentComponent implements AfterViewInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  submit() {
+    if(this.formGroup.invalid) {
+      this.formGroup.markAllAsTouched()
+      return
+    }
+    this.sendRequest()
   }
 }
